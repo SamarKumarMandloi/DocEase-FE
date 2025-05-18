a0dfd1e5c6151eb832cfbfb541c6b3d3dea52ff5
SamarKumarMandloi <samarmandloi19@gmail.com>
2025-05-18 22:32:00 +0530

    add frontend on main

---
+++ src/components/Footer.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='md:mx-10'>
+      <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10  mt-40 text-sm'>
+
+        <div>
+          <h1 className='text-xl font-medium mb-5' >DocEase</h1>
+          <p className='w-full md:w-2/3 text-gray-600 leading-6'>DocEase is a comprehensive web-based platform that addresses the inefficiencies of traditional doctor appointment systems by providing a unified solution for patients, doctors, and administrators.</p>
+        </div>
+
+        <div>
+          <p className='text-xl font-medium mb-5'>COMPANY</p>
+          <ul className='flex flex-col gap-2 text-gray-600'>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy policy</li>
+          </ul>
+        </div>
+
+        <div>
+          <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+          <ul className='flex flex-col gap-2 text-gray-600'>
+            <li>+91-212-456-7890</li>
+            <li>docease@gmail.com</li>
+          </ul>
+        </div>
+
+      </div>
+
+      <div>
+        <hr />
+        <p className='py-5 text-sm text-center'>Copyright 2024 @ DocEase.com - All Right Reserved.</p>
+      </div>
+
+    </div>
+  )
+}
+
+export default Footer
